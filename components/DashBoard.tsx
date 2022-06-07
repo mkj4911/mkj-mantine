501806264af440aaac7beeed86c464ec8045583e
mkj <mkj4911@gmail.com>
2022-06-07 17:28:51 +0900

    form multiselect etc

---
+++ components/DashBoard.tsx
@@ -0,0 +1,65 @@
+import { ShieldCheckIcon } from '@heroicons/react/solid'
+import { LogoutIcon } from '@heroicons/react/outline'
+import { ActionIcon, Center, Menu } from '@mantine/core'
+import { Settings } from 'tabler-icons-react'
+import { supabase } from '../utils/supabase'
+import { Layout } from '../components/Layout'
+import { NextLink } from '@mantine/next'
+
+export const DashBoard = () => {
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+  return (
+    <Layout title="DashBoard">
+      <Center>
+        <ShieldCheckIcon className="mb-4 h-14 w-14 text-gray-300" />
+      </Center>
+      <Center>
+        <Menu trigger="hover" size="xl">
+          <Menu.Label>UI Components</Menu.Label>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/button"
+          >
+            Button
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/grid"
+          >
+            Grid
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/group"
+          >
+            Group
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/multi-select"
+          >
+            Multi Select
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/fetch-api"
+          >
+            Data Loader
+          </Menu.Item>
+        </Menu>
+      </Center>
+      <Center>
+        <ActionIcon my="md" size="lg" onClick={signOut}>
+          <LogoutIcon />
+        </ActionIcon>
+      </Center>
+    </Layout>
+  )
+}
---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type Form = {
+  email: string
+  password: string
+  age: number
+}
+export type Todo = {
+  id: number
+  created_at: string
+  title: string
+}
