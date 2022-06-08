5350a32da2a2a31ffd9d029fc820d51b07937a2c
mkj <mkj4911@gmail.com>
2022-06-09 06:21:38 +0900

    avatar

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -67,6 +67,13 @@ export const DashBoard = () => {
           >
             Modal
           </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/avatar"
+          >
+            Avatar
+          </Menu.Item>
         </Menu>
       </Center>
       <Center>
